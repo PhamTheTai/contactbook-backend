815392d17c03410067b59d85d3d4b2ec4d85d875
PhamTheTai <taib1910571@student.ctu.edu.vn>
2024-03-06 21:55:20 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoBD = require("./app/utils/mongodb.util");
 
-//start server
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is running on post ${PORT}.`);
-});
+
+async function startSever() {
+    try {
+        await MongoBD.connect(config.db.uri);
+        console.log("Connected to the database!");
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running on post ${PORT}.`);
+        });
+    }catch (error) {
+        console.log("Cannot connect to the database!", error);
+        process.exit();
+    }
+}
+startSever();
+
